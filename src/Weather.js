cc03cd6573fa5544d7117690a16dbeaaff84d5f0
nclaire29 <nastasia.claire89@gmail.com>
2021-11-06 18:26:18 -0400

    Updated icon so it changes with city
    interpolated the icon url with the open weather link and added API path - response.data.weather[0].icon

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props){
             city: response.data.name,
             humidity: response.data.main.humidity,
             date: new Date(response.data.dt * 1000),  
-            icon: "https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
+            icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             description: response.data.weather[0].description
         });
     }
