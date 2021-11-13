653d8e8f9bc8111fa4269725aa576ba57ba8128d
nclaire29 <nastasia.claire89@gmail.com>
2021-11-12 22:44:37 -0500

    Created 1-day forecast
    1. Weatherforecast - conditional rendering - if data is loaded, then return the forecast JSX else make the API call
    2. Create useStates loaded, setLoaded & forecast, setForecast
    3. Call & update States of setForecast & setLoaded under funtion handleResponse
    4. Change placeholder data (temp/day) to {forecast[0].temp.max} - ex
    5. Create new component WeatherForecastDay.js - and paste the JSX containing max/min temps & day & wrap in div w/ root element.
    6. Move WeatherIcon component to WFD.js
    7. Chang the temp/day to {props.data.dt} - ex:
    8. Create functions maxTemperature() & minTemperature - create variables temperature and set to math.round{props.data.max/min}. Call these functions w/in JSX
    Create function day() - create variables date & day, create days array and return days[day]

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,38 +1,38 @@
-import React from "react"; 
-import WeatherIcon from "./WeatherIcon"; 
+import React, { useState } from "react";  
 import "./WeatherForecast.css"; 
-import axios from "axios"; //8. Add import axios line 
+import axios from "axios"; //Add import axios line 
+import WeatherForecastDay from "./WeatherForecastDay"; //8. Import WeatherForecastDay 
 
-export default function WeatherForecast(props){ //11. Send coordinates as props
-    function handleResponse(response){ //7. Define function from axios call 
+export default function WeatherForecast(props){//Send coordinates as props  
+    let [loaded, setLoaded] = useState(false); //2. Create State to define loaded
+    let [forecast, setForecast] = useState(null);//3. Create this b/c response parameter of handleResponse doesn't work outside of function
+    
+    function handleResponse(response){  
         console.log(response.data);
+        setForecast(response.data.daily); //4. This is to help store the API path as variable forecast 
+        setLoaded(true); //5. Changes state of loaded from false to true and now forecast will show on app
     }
     
-    let apiKey = "ab6da5069e5bc23122a387b3e99bd05b"; //3. c/p apiKey from Weather.js
-    let longitude = props.coordinates.lon; //12. Store API path of object as longitude/latitude variables 
-    let latitude = props.coordinates.lat; //12. Store API path of object as longitude/latitude variables
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&units=metric`; //2. Store API call in variable, set units to metric  //5. Interpolate #3 & #4 into apiUrl
-
-    axios.get(apiUrl).then(handleResponse);//6. Make axios call and call function
-    
-    return (
+    if (loaded) {//9. Add <WeatherForecastDay data={forecast[0]}/> (data sent as props) under <div className="col"> //10. Cut/paste the original weather JSX (max/min temp and date/time) and move to WFD.js  
+        console.log(forecast);
+        return (
         <div className="WeatherForecast">
             <div className="row">
                 <div className="col">
-                    <div className="WeatherForecast-day">
-                      Thu  
-                    </div>
-                    <WeatherIcon code="01d" size={36} />
-                    <div className="WeatherForecast-temperatures">
-                        <span className="WeatherForecast-temperatures-max">
-                         19°
-                        </span>
-                        <span className="WeatherForecast-temperatures-min">
-                          10°
-                        </span> 
-                    </div>
+                    <WeatherForecastDay data={forecast[0]} />     
                 </div>
             </div>
         </div>
     );
+
+} else{
+    let apiKey = "ab6da5069e5bc23122a387b3e99bd05b"; //C/P apiKey from Weather.js
+    let longitude = props.coordinates.lon; //Store API path of object as longitude/latitude variables 
+    let latitude = props.coordinates.lat; //Store API path of object as longitude/latitude variables
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&units=metric`; //2. Store API call in variable, set units to metric  //5. Interpolate #3 & #4 into apiUrl
+
+    axios.get(apiUrl).then(handleResponse);//Make axios call and call function
+
+    return null;    
+    }   
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,40 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon"; //12. Import WeatherIcon component
+
+export default function WeatherForecastDay(props){ //9. Send data of the first item in array's weather as props
+    function maxTemperature(){ //13. Create function maxTemp - set variable temp = rounded API path and return that temp variable 
+        let temperature = Math.round(props.data.temp.max);
+        return `${temperature}°`;
+    }
+
+    function minTemperature() { //13. Create function minTemp - set variable temp = rounded API path and return that temp variable 
+        let temperature = Math.round(props.data.temp.min);
+        return `${temperature}°`;
+    }
+
+    function day(){ //15. Create day function 
+        let date = new Date(props.data.dt * 1000); //15a. Set date variable - (API path*1000) to get correct day 
+        let day = date.getDay(); //15b. this will get the correct day but as # in array
+
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]; //16. To display day name, create array
+        
+        return days[day]; //17. This will display the day name based on # item in array ex: 6=Sat
+    }
+
+    return ( //10. Wrap JSX from WF.js in <div className="WeatherForecastDay"></div> //11. Change each forecast[0]to props.data.
+        <div className="WeatherForecastDay">
+            <div className="WeatherForecast-day">
+                {day()}
+            </div>
+            <WeatherIcon code={props.data.weather[0].icon} size={36} />
+            <div className="WeatherForecast-temperatures">
+                <span className="WeatherForecast-temperatures-max">
+                    {maxTemperature()}
+                </span>
+                <span className="WeatherForecast-temperatures-min">
+                    {minTemperature()}
+                </span>
+            </div>
+        </div>
+    );
+}
