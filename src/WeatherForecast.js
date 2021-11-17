050d27adec6f8c4646332991dd010f1480f38f8d
nclaire29 <nastasia.claire89@gmail.com>
2021-11-17 00:52:00 -0500

    Got forecast weather working and retouched some code
    - useEffect - allows you to execute code after component is rendered
    - add ELSE { return null; } in forecast.map
    - moved API/geo data into function load() and return load() as else statement

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,19 +1,34 @@
-import React, { useState } from "react";  
+import React, { useState, useEffect } from "react"; //1. Add useEffect to import line
 import "./WeatherForecast.css"; 
-import axios from "axios"; //Add import axios line 
-import WeatherForecastDay from "./WeatherForecastDay"; //8. Import WeatherForecastDay 
+import axios from "axios"; 
+import WeatherForecastDay from "./WeatherForecastDay";  
 
 export default function WeatherForecast(props){//Send coordinates as props  
-    let [loaded, setLoaded] = useState(false); //2. Create State to define loaded
-    let [forecast, setForecast] = useState(null);//3. Create this b/c response parameter of handleResponse doesn't work outside of function
+    let [loaded, setLoaded] = useState(false); 
+    let [forecast, setForecast] = useState(null);
+
+
+    useEffect(() => { //2. useEffect lets you run additional code after component rendered if something changes
+        setLoaded(false); //3a. code to run after component rendered - this will make API call (after city search)
+    }, [props.coordinates]); //3b. - condition required to run 2a - when the coordinates change (after city search)
+
     
     function handleResponse(response){  
         console.log(response.data);
-        setForecast(response.data.daily); //4. This is to help store the API path as variable forecast 
-        setLoaded(true); //5. Changes state of loaded from false to true and now forecast will show on app
+        setForecast(response.data.daily); 
+        setLoaded(true); 
+    }
+
+    function load(){ //4. Improve readability by storing API/geo data as function 
+        let apiKey = "ab6da5069e5bc23122a387b3e99bd05b"; //C/P apiKey from Weather.js
+        let longitude = props.coordinates.lon; //Store API path of object as longitude/latitude variables 
+        let latitude = props.coordinates.lat; //Store API path of object as longitude/latitude variables
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&units=metric`; //Store API call in variable, set units to metric  //5. Interpolate #3 & #4 into apiUrl
+
+        axios.get(apiUrl).then(handleResponse);//Make axios call and call function
     }
     
-    if (loaded) {//9. Add <WeatherForecastDay data={forecast[0]}/> (data sent as props) under <div className="col"> //10. Cut/paste the original weather JSX (max/min temp and date/time) and move to WFD.js  
+    if (loaded) {
         return (
         <div className="WeatherForecast">
             <div className="row">
@@ -24,20 +39,17 @@ export default function WeatherForecast(props){//Send coordinates as props
                                 <WeatherForecastDay data={dailyForecast} />     
                             </div>
                         );
+                    } else { //1. forecast.map(function) must return something if first condition (index <5) is NOT met
+                        return null;
                     }
                 })}   
             </div>
         </div>
     );
 
-} else{
-    let apiKey = "ab6da5069e5bc23122a387b3e99bd05b"; //C/P apiKey from Weather.js
-    let longitude = props.coordinates.lon; //Store API path of object as longitude/latitude variables 
-    let latitude = props.coordinates.lat; //Store API path of object as longitude/latitude variables
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&units=metric`; //2. Store API call in variable, set units to metric  //5. Interpolate #3 & #4 into apiUrl
-
-    axios.get(apiUrl).then(handleResponse);//Make axios call and call function
-
+} else{ //4. Return load function as ELSE statement, will make the API call  
+    load();
+    
     return null;    
     }   
 }
